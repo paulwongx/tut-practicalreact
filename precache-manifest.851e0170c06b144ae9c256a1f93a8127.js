04df7481872a558754f10a15937fe9e65155a138
paulwongx <20178251+paulwongx@users.noreply.github.com>
2020-03-27 22:19:01 -0400

    Updates

---
+++ precache-manifest.851e0170c06b144ae9c256a1f93a8127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b714170f99e34755dd44a4dd9e21b3",
+    "url": "/tut-practicalreact/index.html"
+  },
+  {
+    "revision": "3f323a496a8b90a0f0b9",
+    "url": "/tut-practicalreact/static/css/2.63a747a6.chunk.css"
+  },
+  {
+    "revision": "9301884d30060aec7799",
+    "url": "/tut-practicalreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f323a496a8b90a0f0b9",
+    "url": "/tut-practicalreact/static/js/2.8af50b82.chunk.js"
+  },
+  {
+    "revision": "aadc21a4f78ea00f71eb976f5752fc1f",
+    "url": "/tut-practicalreact/static/js/2.8af50b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9301884d30060aec7799",
+    "url": "/tut-practicalreact/static/js/main.2544646c.chunk.js"
+  },
+  {
+    "revision": "1776a7747bb34eca2939",
+    "url": "/tut-practicalreact/static/js/runtime-main.e4f00522.js"
+  }
+]);
