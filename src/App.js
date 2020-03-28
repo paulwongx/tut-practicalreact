cc7e88ec3e214797646118f89be94e42c7e3a90a
paulwongx <20178251+paulwongx@users.noreply.github.com>
2020-03-27 22:21:07 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Tooltip from './components/Tooltip' // Part 5 - Tooltip
 import './App.css';
 import Countup from './components/Countup'; // Part 6 - CountUp Animated
 import IdleTimerContainer from './components/IdleTimerContainer' // Part 7 - IdleTimerContainer
-import ColorPicker from './components/ColorPicker';
-import CreditCard from './components/CreditCard';
-import DatePickerContainer from './components/DatePickerContainer';
+import ColorPicker from './components/ColorPicker'; // Part 8 - Color Picker
+import CreditCard from './components/CreditCard'; // Part 9 - Credit Card
+import DatePickerContainer from './components/DatePickerContainer'; // Part 10 - Date Picker
 
 function App() {
   return (
